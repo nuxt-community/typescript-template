1b8f410b10ec0eddfcee89d2f49aec60be7d7514
Alexandre Chopin <wikeo65@gmail.com>
2016-11-23 18:47:11 +0100

    update config file

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   head: {
-    title: 'Nuxt.js - A minimal framework for server-renderer Vue.js applications',
+    title: "{{ name }}",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', content: 'Nuxt.js is a minimal framework for creating Vue.js applications with server side rendering, code-splitting, hot-reloading, static generation and more!' }
+      { hid: 'description', content: "{{ description }}" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
