d0dde552d4a744bc0b4052a7346adeeeed515bdd
Sébastien Chopin <seb@chopin.io>
2016-11-23 18:59:46 +0100

    Update meta.js

--- meta.js
+++ meta.js
@@ -21,5 +21,5 @@ module.exports = {
       'message': 'Author'
     },
   },
-  completeMessage: '{{#inPlace}}To get started:\n\n  mv .env.example .env\n  npm install # Or yarn install\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  mv .env.example .env\n  npm install # Or yarn install\n  npm run dev{{/inPlace}}'
+  completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn install\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn install\n  npm run dev{{/inPlace}}'
 };
