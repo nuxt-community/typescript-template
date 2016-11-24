2304c4d6c341af5f4d1370e32934707e2fd8a635
Sébastien Chopin <seb@chopin.io>
2016-11-24 16:21:45 +0100

    use simple quotes

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   head: {
-    title: "{{ name }}",
+    title: '{{ name }}',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
