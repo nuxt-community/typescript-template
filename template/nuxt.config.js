c681a6e92c22fdd005ecd6c02dc1057b7c053ef0
John Lindquist <johnlindquist@gmail.com>
2017-04-10 10:23:43 -0600

    Update nuxt.config.js

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -20,19 +20,11 @@ module.exports = {
   /*
   ** Build configuration
   */
+  css: [
+    'tachyons/css/tachyons.min.css',
+    '~assets/css/main.css'
+  ],
   build: {
-    /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+
   }
 }
