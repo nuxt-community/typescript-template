f5d802ef7c9dd84b2f39d722ef9bbd0310941bb6
Tony Won <jee@hyuk.io>
2018-03-17 06:13:55 +0900

    Add properties in ComponentOptions Type
    Add ‘head’ and ‘layout’ properties in ComponentOptions Type

--- template/index.d.ts
+++ template/index.d.ts
@@ -1,5 +1,13 @@
+import Vue from 'vue'
+
 declare module '*.vue' {
-  import Vue from 'vue'
   const _default: Vue
   export default _default
+}
+
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    head?: any
+    layout?: string
+  }
 }
