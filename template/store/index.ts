cdbdd7ddf78bc29728b69574ee933e331690222d
David Berning <dberning@Daves-MacBook-Pro.local>
2018-11-27 18:13:18 -0500

    improved store typings

--- template/store/index.ts
+++ template/store/index.ts
@@ -1,4 +1,4 @@
-import { RootState } from "~/types";
+import { RootState, Person } from "~/types";
 import { MutationTree, ActionTree } from "vuex";
 
 export const state = (): RootState => ({
@@ -6,14 +6,14 @@ export const state = (): RootState => ({
 })
 
 export const mutations: MutationTree<RootState> = {
-  setPeople(state, people) {
+  setPeople(state: RootState, people: Person[]): void {
     state.people = people
   }
 }
 
 export const actions: ActionTree<RootState, RootState> = {
   async nuxtServerInit({ commit }, { app }) {
-    const people = await app.$axios.$get(
+    const people: Person[] = await app.$axios.$get(
       "./random-data.json"
     )
     commit("setPeople", people.slice(0, 10))
