d3a20fb04b15dcf88e90c591b183eb83d6b32f81
hmsk <k.hamasaki@gmail.com>
2020-07-26 15:15:00 -0700

    Delete template/, Setup new Nuxt project on the root

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+export default {
+  build: {},
+  buildModules: ["@nuxt/typescript-build"],
+  css: [],
+  env: {},
+  head: {
+    title: "nuxt-community/typescript-template",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "A boilerplate to start a Nuxt+TS project quickly" }
+    ],
+    link: []
+  },
+  loading: { color: "#0c64c1" },
+  modules: []
+}
--- template/types/state.ts
+++
@@ -1,5 +0,0 @@
-import { Person } from "~/types";
-
-export interface RootState {
-  people: Person[];
-}
