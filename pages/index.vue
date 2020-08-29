0c9dafc78591fccc25e48d9bc8642f6c24dc61aa
hmsk <k.hamasaki@gmail.com>
2020-08-28 23:07:29 -0700

    Add index page

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li>
+      <nuxt-link to="/options-api">
+        With Options API
+      </nuxt-link>
+    </li>
+    <li>
+      <nuxt-link to="/class-api">
+        With Class API
+      </nuxt-link>
+    </li>
+    <li>
+      <nuxt-link to="/composition-api">
+        With Composition API <small>*Experimental</small>
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+</script>
