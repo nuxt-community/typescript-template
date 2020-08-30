6fd1f23984b48b996ab199e092236abdef63494e
hmsk <k.hamasaki@gmail.com>
2020-08-29 17:07:40 -0700

    Accept COMPONENT_API to choose API in init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 delete_composition()
 {
+  rm pages/composition-api.vue
   sed -i.bak '/@nuxtjs\/composition-api/d' package.json
   sed -i.bak '/@nuxtjs\/composition-api/d' nuxt.config.ts
   sed -i.bak "s/'Composition API'//" pages/index.vue
@@ -7,27 +8,51 @@ delete_composition()
 
 delete_class()
 {
+  rm pages/class-api.vue
   sed -i.bak '/nuxt-property-decorator/d' package.json
   sed -i.bak "s/'Class API', //" pages/index.vue
 }
 
 delete_options()
 {
+  rm pages/options-api.vue
   sed -i.bak "s/'Options API', //" pages/index.vue
 }
 
-echo "Which component style do you prefer?"
-select choice in "Options API" "Class API" "Composition API"; do
-  case $choice in
-    'Options API' )     rm pages/{class,composition}-api.vue; delete_composition; delete_class; break;;
-    'Class API' )       rm pages/{options,composition}-api.vue; delete_composition; delete_options; break;;
-    'Composition API' ) rm pages/{options,class}-api.vue; delete_class; delete_options; break;;
-  esac
-done
+use_options()
+{
+  delete_composition
+  delete_class
+}
 
-rm -f package.json.bak
-rm -f nuxt.config.ts.bak
-rm -f pages/index.vue.bak
-rm init.sh
+use_class()
+{
+  delete_composition
+  delete_options
+}
+
+use_composition()
+{
+  delete_class
+  delete_options
+}
+
+case $COMPONENT_API in
+  options ) use_options;;
+  class ) use_class;;
+  compositon ) use_composition;;
+  *)
+    echo "Which component style do you prefer?"
+    select choice in "Options API" "Class API" "Composition API"; do
+      case $choice in
+        'Options API' )     use_options; break;;
+        'Class API' )       use_class; break;;
+        'Composition API' ) use_composition; break;;
+      esac
+    done
+    ;;
+esac
+
+rm -f package.json.bak nuxt.config.ts.bak pages/index.vue.bak init.sh
 
 echo "Initialized with your choice!\n\nRun npm/yarn install, then commit all changes"
